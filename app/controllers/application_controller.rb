35007a1bb60a913d1e3e840d38a0ec5eb830bd01
motestt65677 <pkpk99887766@gmail.com>
2018-02-12 17:15:50 +0800

    install Clearance gem for user management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+	
+  include Clearance::Controller
   protect_from_forgery with: :exception
 end
+# class ArticlesController < ApplicationController
+#   before_action :require_login
+
+#   def index
+#     current_user.articles
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# Blog::Application.routes.draw do
+#   constraints Clearance::Constraints::SignedIn.new { |user| user.admin? } do
+#     root to: "admin/dashboards#show", as: :admin_root
+#   end
+
+#   constraints Clearance::Constraints::SignedIn.new do
+#     root to: "dashboards#show", as: :signed_in_root
+#   end
+
+#   constraints Clearance::Constraints::SignedOut.new do
+#     root to: "marketing#index"
+#   end
+# end
