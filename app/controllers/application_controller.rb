41d8293c541321fe158ac0b417aee9cf9177d32f
motestt65677 <pkpk99887766@gmail.com>
2018-02-13 10:44:03 +0800

    installed Figaro gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
 	
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+
 end
 # class ArticlesController < ApplicationController
 #   before_action :require_login
