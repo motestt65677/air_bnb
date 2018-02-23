5495a0cf482c83243d52a164bd36057eb995c22c
motestt65677 <pkpk99887766@gmail.com>
2018-02-23 19:45:58 +0800

    change role default to moderator

---
+++ db/migrate/20180223113811_change_user_role_default.rb
@@ -0,0 +1,7 @@
+class ChangeUserRoleDefault < ActiveRecord::Migration[5.1]
+  def change
+    change_column :users, :role, :integer, :default => 0
+    
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222115420) do
+ActiveRecord::Schema.define(version: 20180223113811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,7 @@ ActiveRecord::Schema.define(version: 20180222115420) do
     t.string "phone"
     t.string "country"
     t.date "birthdate"
-    t.integer "role"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
